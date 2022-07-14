2f6c595c45f3fddb58fd73dd44429dcb43f86a6b
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-13 23:09:04 -0600

    add files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './index.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      
+      
+      
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/portafolio/Portafolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './portafolio.css';
+
+
+const Portafolio = () => {
+  return (
+    <div>Portafolio</div>
+  )
+}
+
+export default Portafolio
