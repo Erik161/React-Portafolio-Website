e2c15cdcfc1a1a2a6db01fc03035d6abba342277
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-13 23:53:22 -0600

    Managing and Creating files & Folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react';
+import React from 'react';
 import './index.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portafolio from './components/portafolio/Portafolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   
 
   return (
-    <div className="App">
-      
-      
-      
+    <div>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portafolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
       
     </div>
   )
