71a756b0379665e39eb8cbd345d418b76c9bbebe
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-15 22:32:38 -0600

    add Header Components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hola soy</h5>
+        <h1>Erik Hernández López</h1>
+        <h5 className='text-light' >Fullstack Developer</h5>
+        <CTA/>
+        <div className='me'>
+          
+        </div>
+      </div>
+    </header>
   )
 }
 
