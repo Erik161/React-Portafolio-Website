71b9dd2eb18c6868a3e5ae40b4412ec62e7bf31d
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-29 20:35:57 -0600

    Experience Component JSX

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './experience.css';
-
+import {BsPatchCheckFill} from 'react-icons/bs';
 const Experience = () => {
   return (
-    <section id='experiencie'>Experience</section>
+    <section id='experiencie'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+            <BsPatchCheckFill/>
+            <h4>HTML</h4>
+            <small className='text-light' >Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        <div className='experience__backend'>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
