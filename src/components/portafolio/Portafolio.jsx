a281195552e299f39affc5d2ae4d648ea23665d0
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-10-12 23:32:10 -0600

    Portfolio Component JSX

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -4,7 +4,20 @@ import './portafolio.css';
 
 const Portafolio = () => {
   return (
-    <section id='portfolio' >Portafolio</section>
+    <section id='portfolio' >
+      <h5>My Recent Work</h5>
+      <h2>Portafolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+          </div>
+          <h3>Title</h3>
+          <a href='' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Live Demo </a>
+        </article>
+      </div>
+    </section>
   )
 }
 
