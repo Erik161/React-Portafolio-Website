59c8721b29f2ee2de269857f31627ebf1296aea2
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-11-07 21:06:11 -0600

    Generating PortFolio Items From Array Using Map method

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -3,6 +3,68 @@ import './portafolio.css';
 import port1 from '../../assets/port1.jpg';
 
 
+
+// DO NOT USE THE IMAGES IN PRODUCTION
+
+const data =[
+  {
+    id:1,
+    image:IMG1,
+    title:'Proyect Landing Page',
+    github:'https://github.com/Erik161/ReactMusicAppLandingPage',
+    demo:'https://erik161.github.io/ReactMusicAppLandingPage/',
+  },
+
+  {
+    id:2,
+    image:IMG2,
+    title:'Proyect Landing Page',
+    github:'https://github.com/Erik161/ReactMusicAppLandingPage',
+    demo:'https://erik161.github.io/ReactMusicAppLandingPage/',
+  },
+
+  {
+    id:3,
+    image:IMG3,
+    title:'Proyect Landing Page',
+    github:'https://github.com/Erik161/ReactMusicAppLandingPage',
+    demo:'https://erik161.github.io/ReactMusicAppLandingPage/',
+  },
+
+
+  {
+    id:4,
+    image:IMG4,
+    title:'Proyect Landing Page',
+    github:'https://github.com/Erik161/ReactMusicAppLandingPage',
+    demo:'https://erik161.github.io/ReactMusicAppLandingPage/',
+  },
+
+  
+  {
+    id:5,
+    image:IMG4,
+    title:'Proyect Landing Page',
+    github:'https://github.com/Erik161/ReactMusicAppLandingPage',
+    demo:'https://erik161.github.io/ReactMusicAppLandingPage/',
+  },
+
+
+  
+  {
+    id:6,
+    image:IMG4,
+    title:'Proyect Landing Page',
+    github:'https://github.com/Erik161/ReactMusicAppLandingPage',
+    demo:'https://erik161.github.io/ReactMusicAppLandingPage/',
+  },
+
+ 
+]
+
+
+
+
 const Portafolio = () => {
   return (
     <section id='portfolio' >
@@ -19,64 +81,7 @@ const Portafolio = () => {
             <a href='https://github.com/Erik161/ReactMusicAppLandingPage' className='btn' target='_blank'>Github</a>
             <a href='https://erik161.github.io/ReactMusicAppLandingPage/' className='btn btn-primary' target='_blank'>Live Demo </a>
           </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={port1} alt="" />
-          </div>
-          <h3>Title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/Erik161/ReactMusicAppLandingPage' className='btn' target='_blank'>Github</a>
-            <a href='https://erik161.github.io/ReactMusicAppLandingPage/' className='btn btn-primary' target='_blank'>Live Demo </a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={port1} alt="" />
-          </div>
-          <h3>Title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/Erik161/ReactMusicAppLandingPage' className='btn' target='_blank'>Github</a>
-            <a href='https://erik161.github.io/ReactMusicAppLandingPage/' className='btn btn-primary' target='_blank'>Live Demo </a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={port1} alt="" />
-          </div>
-          <h3>Title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/Erik161/ReactMusicAppLandingPage' className='btn' target='_blank'>Github</a>
-            <a href='https://erik161.github.io/ReactMusicAppLandingPage/' className='btn btn-primary' target='_blank'>Live Demo </a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={port1} alt="" />
-          </div>
-          <h3>Title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/Erik161/ReactMusicAppLandingPage' className='btn' target='_blank'>Github</a>
-            <a href='https://erik161.github.io/ReactMusicAppLandingPage/' className='btn btn-primary' target='_blank'>Live Demo </a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={port1} alt="" />
-          </div>
-          <h3>Title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/Erik161/ReactMusicAppLandingPage' className='btn' target='_blank'>Github</a>
-            <a href='https://erik161.github.io/ReactMusicAppLandingPage/' className='btn btn-primary' target='_blank'>Live Demo </a>
-          </div>
-        </article>
-
-        
+        </article> 
       </div>
     </section>
   )
