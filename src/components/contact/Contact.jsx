00a9c97e010d073f7b64df09febad12df42fd090
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-01-30 22:32:05 -0600

    Contact Component JSX (Multiple Contact options: Facebook Messenger, Whatsapp & Email)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import {RiMessengerLine} from 'react-icons/ri';
+import {BsWhatsapp} from 'react-icons/bs';
 
 const Contact = () => {
   return (
     <section id='contact' >
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>erikestuardo_161@hotmail.com</h5>
+            <a href='mailto:erikestuardo_161@hotmail.com' target="_blank">Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine/>
+            <h4>Messenguer</h4>
+            <h5>Erik Hernández</h5>
+            <a href='https://m.me/erik.hernandez' target="_blank">Send a message</a>
+          </article>
+
+
+          <article className='contact__option'>
+            <BsWhatsapp/>
+            <h4>Whatsapp</h4>
+            <h5>erikestuardo_161@hotmail.com</h5>
+            <a href='https://api.whatsapp.com/send?phone=50236074761' target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+
+        <form action=''>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
     </section>
   )
 }
