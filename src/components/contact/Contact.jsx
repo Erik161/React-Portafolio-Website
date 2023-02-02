70a16a7289f1b32fcdd130afe53343acec11e65f
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-02-01 18:21:55 -0600

    Contact Component CSS| contact__options modified

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,29 +6,29 @@ import {BsWhatsapp} from 'react-icons/bs';
 
 const Contact = () => {
   return (
-    <section id='contact' >
+    <section id="contact" >
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
-      <div className='container contact__container'>
+      <div className="container contact__container">
 
-        <div className='contact__options'>
-          <article className='contact__option'>
-            <MdOutlineEmail className='contact__option-icon' />
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>erikestuardo_161@hotmail.com</h5>
             <a href='mailto:erikestuardo_161@hotmail.com' target="_blank">Send a message</a>
           </article>
 
-          <article className='contact__option'>
-            <RiMessengerLine className='contact__option-icon' />
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenguer</h4>
             <h5>Erik Hernández</h5>
             <a href='https://m.me/erik.hernandez' target="_blank">Send a message</a>
           </article>
 
 
-          <article className='contact__option'>
-            <BsWhatsapp className='contact__option-icon' />
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
             <h4>Whatsapp</h4>
             <h5>erikestuardo_161@hotmail.com</h5>
             <a href='https://api.whatsapp.com/send?phone=50236074761' target="_blank">Send a message</a>
@@ -40,7 +40,7 @@ const Contact = () => {
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <button type='submit' className="btn btn-primary">Send Message</button>
         </form>
 
       </div>
