528cf9f0af07a874698e06c460c691dbf72a852f
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-02-01 19:46:31 -0600

    Form Submission using EmailJS|Sing Up Free EmailJS|npm installl emailjs-com --save|Document https://www.emailjs.com/docs/examples/reactjs/

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,26 @@ import './contact.css';
 import {MdOutlineEmail} from 'react-icons/md';
 import {RiMessengerLine} from 'react-icons/ri';
 import {BsWhatsapp} from 'react-icons/bs';
+import {useRef} from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a8s5fp4', 'template_nkagobc', form.current, 'N8PTv1UJyoZcLBJsb')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
+
   return (
     <section id="contact" >
       <h5>Get In Touch</h5>
@@ -36,7 +54,7 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
 
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail} >
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
